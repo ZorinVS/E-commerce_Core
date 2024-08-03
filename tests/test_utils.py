15f2a207d2e1b46589c0a9b6c7c185f0b1ae6794
Vladislav Zorin <zorin.3v6@gmail.com>
2024-08-03 10:40:18 +0300

    Add tests

---
+++ tests/test_utils.py
@@ -0,0 +1,54 @@
+import json
+from unittest.mock import mock_open, patch
+
+from src.utils import make_the_python_objects_from_json
+
+
+def test_make_the_python_objects_from_valid_json(to_json: tuple) -> None:
+    """Тестирует создание объектов из корректного JSON-файла"""
+    json_data = json.dumps(to_json[0])  # [0] - валидные данные
+
+    mocked_open = mock_open(read_data=json_data)
+    with patch("builtins.open", mocked_open):
+        with patch("os.path.exists", return_value=True):
+            categories_utils, products_utils = make_the_python_objects_from_json("dummy_path.json")
+            # Сравнение количества категорий
+            assert len(categories_utils) == len(to_json[0])
+            # Сравнение количества продуктов
+            assert len(products_utils) == sum(len(category["products"]) for category in to_json[0])
+            mocked_open.assert_called_once_with("dummy_path.json")
+
+
+def test_make_the_python_objects_from_json_not_exist() -> None:
+    """Тестирует работу функции с ненайденным файлом"""
+    with patch("os.path.exists", return_value=False) as mock_exists:
+        categories_utils, products_utils = make_the_python_objects_from_json("dummy_path.json")
+        assert categories_utils is None
+        assert products_utils is None
+        mock_exists.assert_called_once_with("dummy_path.json")
+
+
+def test_make_the_python_objects_from_json_invalid_category(to_json: tuple) -> None:
+    """Тестирует работу функции с JSON-файлом, который содержит некорректные данные"""
+    json_data = json.dumps(to_json[1])  # [1] - отсутствует поле 'name' в словаре содержащем, информацию о категории
+
+    mocked_open = mock_open(read_data=json_data)
+    with patch("builtins.open", mocked_open):
+        with patch("os.path.exists", return_value=True):
+            categories_utils, products_utils = make_the_python_objects_from_json("dummy_path.json")
+            assert categories_utils is None
+            assert products_utils is None
+            mocked_open.assert_called_once_with("dummy_path.json")
+
+
+def test_make_the_python_objects_from_json_invalid_product(to_json: tuple) -> None:
+    """Тестирует работу функции с JSON-файлом, который содержит некорректные данные"""
+    json_data = json.dumps(to_json[2])  # [2] - отсутствует поле 'name' в словаре, содержащем информацию о продукте
+
+    mocked_open = mock_open(read_data=json_data)
+    with patch("builtins.open", mocked_open):
+        with patch("os.path.exists", return_value=True):
+            categories_utils, products_utils = make_the_python_objects_from_json("dummy_path.json")
+            assert categories_utils is None
+            assert products_utils is None
+            mocked_open.assert_called_once_with("dummy_path.json")
