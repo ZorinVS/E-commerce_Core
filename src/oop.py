7b31d59926283ea052ee05db740656b84dedd888
Vladislav Zorin <zorin.3v6@gmail.com>
2024-08-08 01:14:48 +0300

    Update oop.py

--- src/oop.py
+++ src/oop.py
@@ -8,13 +8,23 @@ class Product:
     all_products: List["Product"] = list()
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
-        """Метод для инициализации экземпляра класса Product."""
+        """Dunder-метод для инициализации экземпляра класса Product."""
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
 
         Product.all_products.append(self)
+
+    def __str__(self) -> str:
+        """Dunder-метод для строкового отображения информации об объекте."""
+        return f"{self.name}, {round(self.price)} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other: "Product") -> int:
+        """Dunder-метод для сложения товаров и получения полной стоимости всех товаров на складе."""
+        # if not isinstance(other, Product):
+        #     raise ValueError("Складывать можно только экземпляры класса Product")
+        return round(self.price * self.quantity + other.price * other.quantity)
 
     @classmethod
     def new_product(cls, product_dict: dict) -> "Product":
@@ -67,13 +77,18 @@ class Category:
     product_count: int = 0
 
     def __init__(self, name: str, description: str, products: List[Product]) -> None:
-        """Метод для инициализации экземпляра класса Category."""
+        """Dunder-метод для инициализации экземпляра класса Category."""
         self.name = name
         self.description = description
         self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def __str__(self) -> str:
+        """Dunder-метод для строкового отображения информации об объекте."""
+        total = sum(product.quantity for product in self.__products)
+        return f"{self.name}, количество продуктов: {total} шт."
 
     def add_product(self, product: Product) -> None:
         """Метод для добавления продукта в экземпляр класса Category."""
@@ -83,7 +98,31 @@ class Category:
     @property
     def products(self) -> str:
         """Геттер для получения списка продуктов в виде строк."""
-        return "\n".join(
-            f"{product.name}, {round(product.price)} руб. Остаток: {product.quantity} шт."
-            for product in self.__products
-        )
+        return "\n".join(str(product) for product in self.__products)
+
+    @property
+    def products_list(self) -> List[Product]:
+        """Геттер для получения списка продуктов."""
+        return self.__products
+
+
+class CategoryProductsIterator:
+    """Вспомогательный класс для итерации по товарам в категории."""
+
+    def __init__(self, category: Category) -> None:
+        """Dunder-метод для инициализации итератора с объектом категории."""
+        self.__category = category
+        self.__index = 0
+
+    def __iter__(self) -> "CategoryProductsIterator":
+        """Dunder-метод для возврата итератора."""
+        return self
+
+    def __next__(self) -> Product:
+        """Dunder-метод для получения следующего товара в категории."""
+        if self.__index < len(self.__category.products_list):
+            product = self.__category.products_list[self.__index]
+            self.__index += 1
+            return product
+        else:
+            raise StopIteration
